691cdaa910081d546751508ddfa04792a10d8924
Marcelo Rangel <marcelohar@hotmail.com>
2024-03-10 11:46:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ProjetoCPP
+ Aprendendo CPP
---
+++ Teste.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "\nHello, world!";
+
+        int n1 = 3;
+        int n2 = 2;
+    cout << "\nDigite um numero: ";
+    cin >> n2;
+
+    cout << "\nnumero final " << n2;
+
+
+    return 0;
+} 
